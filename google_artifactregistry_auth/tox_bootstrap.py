df9d8fea7efdc613f031b48f1cf489d30ac44330
Dustin Ingram <di@users.noreply.github.com>
2021-05-27 16:38:37 -0400

    Install the same requirement specified in the tox config

--- google_artifactregistry_auth/tox_bootstrap.py
+++ google_artifactregistry_auth/tox_bootstrap.py
@@ -1,17 +1,8 @@
 import pluggy
 
-import google_artifactregistry_auth
-
 hookimpl = pluggy.HookimplMarker("tox")
 
 
 @hookimpl
 def tox_testenv_install_deps(venv, action):
-    venv._install(
-        [
-            "keyrings.google-artifactregistry-auth=={}".format(
-                google_artifactregistry_auth.__version__
-            )
-        ],
-        action=action,
-    )
+    venv._install(venv.envconfig.config.requires, action=action)
